f4dfe07daf74e30d9b5524b87a9c727937aef71b
Pankaj Kumar <pankajkumar199922@gmail.com>
2026-06-09 12:58:03 +0530

    build log test

--- src/main.jsx
+++ src/main.jsx
@@ -90,12 +90,12 @@ const router = createBrowserRouter(
        
     
     createRoot(document.getElementById('root')).render(
-      <StrictMode>
+      
         <Provider store={store}>
        <RouterProvider router={router}/>
     
         </Provider>
-      </StrictMode>,
+     
     )
 
       
