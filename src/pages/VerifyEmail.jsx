697dad958580260ebc04f486aa4cc569fe9960cf
Pankaj Kumar <pankajkumar199922@gmail.com>
2026-06-10 13:29:56 +0530

    build log test

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -2,98 +2,6 @@ import React, { useEffect, useState,useRef } from "react";
 import { useSearchParams, Link, useNavigate } from "react-router-dom";
 import authService from "../appwrite/auth";
 
-// function VerifyEmail() {
-  
-//   const hasRef = useRef(false)
-//   console.log("VERIFY PAGE MOUNTED");
-//   const navigate = useNavigate()
-//   const [searchParams] = useSearchParams();
-
-//   const [status, setStatus] = useState("loading");
-
-//  useEffect(() => {
-//   const verify = async () => {
-//     if (hasRef.current) return;
-//     hasRef.current = true;
-
-//     console.log("VERIFY PAGE OPENED");
-//     console.log("TIME:", new Date().toISOString());
-//      console.log("URL:", window.location.href);
-
-        
-
-//     try {
-//       const userId = searchParams.get("userId");
-//       const secret = searchParams.get("secret");
-
-//       console.log("userId =", userId);
-//       console.log("secret =", secret);
-
-//       if (!userId || !secret) {
-//         setStatus("invalid");
-//         return;
-//       }
-
-//       const result = await authService.verifyEmail(
-//         userId,
-//         secret
-//       );
-
-//       console.log("VERIFY SUCCESS", result);
-
-//       if (result) {
-//         navigate("/verify-success", { replace: true });
-//         return;
-//       }
-
-//       setStatus("error");
-//     } catch (error) {
-//       console.error("Verification Error:", error);
-//       setStatus("error");
-//     }
-//   };
-
-//   verify();
-// }, []);
-
-
-
-
-
-
-
-
-//   return (
-
-//     <div className="min-h-[80vh] flex items-center justify-center px-4">
-
-//       <div className="bg-white rounded-3xl shadow-xl p-8 text-center max-w-md w-full">
-
-//         {status === "loading" && (
-//           <p>Verifying your email...</p>
-//         )}
-
-
-
-//         {status === "error" && (
-//           <>
-//             <h1 className="text-3xl font-bold text-red-600 mb-4">
-//               ❌ Verification Failed
-//             </h1>
-
-//             <p className="text-slate-600">
-//               The verification link is invalid or expired.
-//             </p>
-//           </>
-//         )}
-
-//       </div>
-
-//     </div>
-//   );
-// }
-
-
 
 
 function VerifyEmail() {
@@ -197,4 +105,3 @@ function VerifyEmail() {
 
 export default VerifyEmail;
 
-// export default VerifyEmail;
