0b9c65bea73b519f4bef3c7a3acd9ba0c714ba23
Pankaj Kumar <pankajkumar199922@gmail.com>
2026-06-13 13:04:28 +0530

    Polish authentication system and user interface

--- src/utils/AvatarColor.jsx
+++ src/utils/AvatarColor.jsx
@@ -1,6 +1,6 @@
 
 const avatarColors = [
-  "from-red-500 to-pink-500",
+   "from-red-500 to-pink-500",
   "from-blue-500 to-indigo-500",
   "from-green-500 to-emerald-500",
   "from-purple-500 to-violet-500",
@@ -8,6 +8,14 @@ const avatarColors = [
   "from-cyan-500 to-sky-500",
   "from-teal-500 to-green-500",
   "from-fuchsia-500 to-pink-500",
+  "from-rose-500 to-red-500",
+  "from-lime-500 to-green-500",
+  "from-yellow-500 to-orange-500",
+  "from-violet-500 to-purple-500",
+  "from-sky-500 to-blue-500",
+  "from-emerald-500 to-teal-500",
+  "from-pink-500 to-rose-500",
+  "from-indigo-500 to-blue-500",
    ];
 
 
